2e49dfc67df3fa63596036c0cda7aa6c6efed4fe
Fuzz Worley <bitcopia@gmail.com>
2022-01-14 14:48:33 -0600

    make category classes dynamic in view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    # update at some point to live stores and then featured store
     @stores = Store.all.limit(9)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? "navActive" : ""
-   end
+  end
+
+  def pill_class(category)
+    case category
+    when 'lodging'
+      'bg-success text-dark'
+    when 'active'
+      'bg-danger'
+    when 'explore'
+      'bg-warning text-dark'
+    when 'relaxing'
+      'bg-info text-dark'
+    when 'food__and_drink'
+      'bg-primary'
+    end
+  end
 end
